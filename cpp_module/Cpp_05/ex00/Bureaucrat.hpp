2af3bea557d9399a72da245de1f5d6e0d6066b3f
kko <kko@student.42seoul.kr>
2023-06-28 00:24:45 +0900

    feat: ex00추가

---
+++ cpp_module/Cpp_05/ex00/Bureaucrat.hpp
@@ -0,0 +1,34 @@
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+#include <iostream>
+#include <string>
+#include <exception>
+
+class Bureaucrat
+{
+private:
+    const std::string _name; 
+    int _grade;
+    Bureaucrat();
+public:
+    Bureaucrat(const std::string &name, const int &grade) throw(GradeTooHighException, GradeTooLowException);
+    Bureaucrat(const Bureaucrat &c);
+    Bureaucrat& operator=(const Bureaucrat &c);
+    virtual ~Bureaucrat();
+    std::string getName() const;
+    int getGrade() const;
+    void incrementGrade() throw(GradeTooHighException);
+    void decrementGrade() throw(GradeTooLowException);
+    class GradeTooHighException : public std::exception
+    {
+        virtual const char *what() const throw();
+    };
+    class GradeTooLowException : public std::exception
+    {
+        virtual const char *what() const throw();
+    };
+};
+
+std::ostream& operator<<(std::ostream &o,const Bureaucrat &c);
+
+#endif
--- cpp_module/Cpp_05/ex00/main.cpp
+++ cpp_module/Cpp_05/ex00/main.cpp
@@ -1,6 +1,53 @@
 #include <iostream>
+#include "Bureaucrat.hpp"
 
 int main()
 {
-    
+    try
+    {
+        Bureaucrat aaa("aaa", -1);
+        std::cout << "here" << std::endl;
+    }
+    catch (std::exception &e)
+    {
+        std::cerr << "Err: " << e.what() << std::endl;
+    }
+
+    try
+    {
+        Bureaucrat aaa("aaa", 151);
+        std::cout << "here" << std::endl;
+    }
+    catch (std::exception &e)
+    {
+        std::cerr << "Err: " << e.what() << std::endl;
+    }
+
+    try
+    {
+        Bureaucrat kko("kko", 2);
+        std::cout << kko << std::endl;
+        kko.incrementGrade();
+        std::cout << kko << std::endl;
+        kko.incrementGrade();
+    }
+    catch (std::exception &e)
+    {
+        std::cerr << "Err: " << e.what() << std::endl;
+    }
+
+    try
+    {
+        Bureaucrat kko("kko", 149);
+        std::cout << kko << std::endl;
+        kko.decrementGrade();
+        std::cout << kko << std::endl;
+        kko.decrementGrade();
+    }
+    catch (std::exception &e)
+    {
+        std::cerr << "Err: " << e.what() << std::endl;
+    }
+
+    return (0);
 }
