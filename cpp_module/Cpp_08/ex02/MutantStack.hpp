41f43deda103774ec59c0d0d0a4496c3f116dab2
kko <kko@student.42seoul.kr>
2023-10-18 20:21:30 +0900

    03아직덜댓음

--- cpp_module/Cpp_08/ex02/MutantStack.hpp
+++ cpp_module/Cpp_08/ex02/MutantStack.hpp
@@ -8,10 +8,35 @@
 #include <typeinfo>
 #include <stack>
 
-template <typename type, typename container = std::vector<type>, typename size>
-class MutantStack : public std::stack<type, container, size>
+template <typename T>
+class MutantStack : public std::stack<T, std::vector<T> >
 {
-
+    MutantStack();
+    // ~MutantStack();
+    // MutantStack(const MutantStack &c);
+    // MutantStack& operator=(const MutantStack &c);
+    // template <typename T>
+    std::vector<T>::iter& begin();
+    // template <typename T>
+    typename std::vector<T>::iter& end();
 };
 
+template <typename T>
+MutantStack<T>::MutantStack()
+{
+}
+
+template <typename T>
+typename std::vector<T>::iterator& begin()
+{
+    return c.begin();
+}
+
+template <typename T>
+typename std::vector<T>::iterator& end()
+{
+    return c.end();
+}
+
 #endif
+
--- cpp_module/Cpp_08/ex02/main.cpp
+++ cpp_module/Cpp_08/ex02/main.cpp
@@ -7,12 +7,62 @@
 #include <typeinfo>
 #include <stack>
 
+template <typename T>
+class MutantStack : public std::stack<T, std::vector<T> >
+{
+    public:
+        typedef typename std::vector<T>::iterator iterator;
+        iterator begin();
+        iterator end();
+        void pop();
+};
+
+template <typename T>
+typename std::vector<T>::iterator MutantStack<T>::begin()
+{
+    return this->c.begin();
+}
+
+template <typename T>
+typename std::vector<T>::iterator MutantStack<T>::end()
+{
+    return this->c.end();
+}
+
+template <typename T>
+void MutantStack<T>::pop()
+{
+    this->c.pop_back();
+}
+
+
 int main()
 {
-    std::stack<int,std::vector<int> > a;
-    a.push(188);
-    a.push(18);
-    std::vector<int>::iterator iter = a.c.begin();
-    std::cout << *iter << std::endl;
+    // MutantStack<int> a;
+    // a.push(4);
+    // a.push(3);
+    // MutantStack<int>::iter iter = a.begin();
+    // std::cout << *a.begin() << std::endl;
+
+    MutantStack<int> mstack;
+    mstack.push(5);
+    mstack.push(17);
+    std::cout << mstack.top() << std::endl;
+    mstack.pop();
+    std::cout << mstack.size() << std::endl;
+    mstack.push(3);
+    mstack.push(5);
+    mstack.push(737);
+    MutantStack<int>::iterator it = mstack.begin();
+    MutantStack<int>::iterator ite = mstack.end();
+    ++it;
+    --it;
+    while (it != ite)
+    {
+        std::cout << *it << std::endl;
+        ++it;
+    }
+    // std::stack<int> s(mstack);
+    return 0;
 }
 
