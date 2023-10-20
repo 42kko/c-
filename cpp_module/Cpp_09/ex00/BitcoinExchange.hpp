cf5a7b21b05d12912899a3c6c7212de0ecf5853b
kko <kko@student.42seoul.kr>
2023-10-20 13:45:19 +0900

    feat cpp09 ex00

---
+++ cpp_module/Cpp_09/ex00/BitcoinExchange.hpp
@@ -0,0 +1,20 @@
+#ifndef BITCOINEXCHANGE_HPP
+#define BITCOINEXCHANGE_HPP
+#include <map>
+#include <string>
+#include <iostream>
+#include <fstream>
+
+class bitcoinExchange
+{
+    private:
+
+    public:
+        bitcoinExchange();
+        ~bitcoinExchange();
+        bitcoinExchange(const bitcoinExchange &c);
+        bitcoinExchange& operator=(const bitcoinExchange &c);
+
+};
+
+#endif
